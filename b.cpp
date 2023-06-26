ace46f5b23191c4bc32621cb7a33005604d9fd5a
angela <angela@userde-MacBook-Pro.local>
2023-06-26 20:43:55 +0800

    626

--- b.cpp
+++ b.cpp
@@ -12,21 +12,32 @@ struct node{
     int key;
     struct node *next;
 };
-struct hash_item{
-    int value;
-    int key;
-};
 struct hash_table{
-    int size;
-    int count;
+    node *arr[20];
 };
-int hash_f(int key){
+
+int hash_f(int);
+void initial_arr(hash_table*);
+node* create_node(void);
+void keyinValue(node*,int,int);
+node* add_node(node*,int,int);
+void put_data(hash_table*,int,int);
+void hash_dump(hash_table*);
+void linked_list_dump(node*);
+
+int
+hash_f(int key){
     return key % 20;
 }
-node* index_array[20];
-
+void
+initial_arr(hash_table *table_root){
+    for(int i = 0;i < 20;i++){
+        table_root->arr[i] = 0;
+    }
+    return;
+}
 node*
-create_node(){
+create_node(void){
     return (node*)malloc(sizeof(node));
 }
 void
@@ -56,40 +67,55 @@ add_node(node *root, int k,int v){
     }
     return root;
 }
-
 void
-output(node* root){
-    node* current = root;
-    while(current != NULL){
-        cout << current->key << "," << current->value << endl;
-        current = current->next;
+put_data(hash_table *table_root,int key,int value){
+    node *new_root = NULL;
+    int index = hash_f(key);
+//if arr[index]==NULL
+    if(table_root->arr[index] == 0){
+        new_root = add_node(new_root,key,value);
+        table_root->arr[index] = new_root; 
+        return;
     }
+//if already exist,append
+    table_root->arr[index] = add_node(table_root->arr[index],key,value);
+    return;
 }
+
 int
 main(){
     node* root = NULL;
-    memset(index_array,'/0',20);
+    struct hash_table table;
+    struct hash_table* table_root = &table;
+    initial_arr(table_root);
     int k[3] = {1,2,3};
     int v[3] = {10,20,30};
-    for(int i = 0;i < 5;i++){
-        store_data(k[i],v[i]);
+    for(int i = 0;i < 3;i++){
+        put_data(table_root,k[i],v[i]);
     }
-    // for(int i = 0;i < 5;i++){
-    //     root = add_node(root,a[i],a[i+1]);
-    //     i++;
-    // }
-    // output(root);
+    hash_dump(table_root);
+
+    for(int i = 0;i < 20;i++){
+        cout << table_root->arr[i] << endl;
+    }
 
     return 0;
 }
-node* store_data(int key,int value){
-    node* root;
-    int index = hash_f(key);
-    root = index_array[index];
-    if(index_array[index] == NULL){
-        node* root = NULL;
-        index_array[index] = add_node(root,key,value);
-        return index_array[index];
+void
+hash_dump(hash_table *table_root){
+    int i = 0;
+    while(i < 20){
+        linked_list_dump(table_root->arr[i]);
+        i++;
     }
-    return add_node(root,key,value);
+    return;
+}
+void
+linked_list_dump(node *root){
+    node *current = root;
+    while(current != NULL){
+        cout << "key: " << current->key << " value: " << current->value << endl;
+        current = current->next;
+    }
+    return;
 }
